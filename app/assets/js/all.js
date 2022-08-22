9229509fb02a6475baa74cce4c1a32155f3da864
Sec <qianqian1903@gmail.com>
2022-08-22 22:32:08 +0800

    update for checkout page

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,7 @@
 const mobileNavBtn = document.querySelector("#mobile-nav-btn");
 const mobileMenu = document.querySelector("#mobile-menu");
+const loginBtn = document.querySelector("#login");
+const modal = document.querySelector("#modal");
 
 mobileNavBtn.addEventListener("click", () => {
     const elHeight = mobileMenu.scrollHeight;
@@ -13,4 +15,15 @@ mobileNavBtn.addEventListener("click", () => {
         mobileMenu.dataset.collapsed = "true";
     }
     // expandMenu(mobileMenu);
+});
+
+loginBtn.addEventListener("click", () => {
+    // document.body.classList.add("fixed-modal");
+    document.body.style.overflow = "hidden";
+    document.body.style.paddingRight = "17px";
+    modal.showModal();
+});
+
+const datepicker = new Datepicker(document.querySelector("#reserve-date"), {
+    orientation: "bottom auto",
 });
