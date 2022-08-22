0fe02323ff79e8aecf68fe166ebfbaf3e7dc5059
Sec <qianqian1903@gmail.com>
2022-08-22 22:32:34 +0800

    Update 2022-08-22T14:32:31.618Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,6 +2,8 @@
 
 var mobileNavBtn = document.querySelector("#mobile-nav-btn");
 var mobileMenu = document.querySelector("#mobile-menu");
+var loginBtn = document.querySelector("#login");
+var modal = document.querySelector("#modal");
 mobileNavBtn.addEventListener("click", function () {
   var elHeight = mobileMenu.scrollHeight;
   var isCollapsed = mobileMenu.dataset.collapsed;
@@ -14,5 +16,14 @@ mobileNavBtn.addEventListener("click", function () {
     mobileMenu.dataset.collapsed = "true";
   } // expandMenu(mobileMenu);
 
+});
+loginBtn.addEventListener("click", function () {
+  // document.body.classList.add("fixed-modal");
+  document.body.style.overflow = "hidden";
+  document.body.style.paddingRight = "17px";
+  modal.showModal();
+});
+var datepicker = new Datepicker(document.querySelector("#reserve-date"), {
+  orientation: "bottom auto"
 });
 //# sourceMappingURL=all.js.map
